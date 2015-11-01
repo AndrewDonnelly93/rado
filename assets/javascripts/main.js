f6661b326b07b8d26d2e366ed3dd6aa494361d25
AndrewDubovtsev <andrey.dubovtsev@gmail.com>
2015-11-01 17:18:54 +0300

    js begin

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,6 +1,34 @@
-$(document).ready(function() {
+function setPositionToWatch(watchSection, watch, largeScreen) {
+  var watchSectionTopOffset = $(watchSection).offset().top;
+  var nextSiblingTopOffset = $(watchSection).next().offset().top;
+  $(document).on("scroll",function() {
+    if (($(this).scrollTop() >= watchSectionTopOffset)
+      && ($(this).scrollTop() <= nextSiblingTopOffset)) {
+      var largeScreenMatches = window.matchMedia("screen and (min-width:"+largeScreen+")");
+      if (largeScreenMatches.matches) {
+
+      } else {
+
+      }
+    }
+  });
+}
+
+$(function() {
+
   $(".fancybox").fancybox({
     openEffect	: 'none',
     closeEffect	: 'none'
   });
+
+  if ($(".faq-container").length && $(".watch-image").length) {
+    var watchSection = ".faq-container";
+    var watch = ".watch-image";
+    var largeScreen = 1903;
+    setPositionToWatch(watchSection, watch, largeScreen);
+    $(window).resize(function () {
+      setPositionToWatch(watchSection, watch, largeScreen);
+    });
+  }
+
 });
